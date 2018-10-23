58619a423a58803438c23d0712ea15234d094fd2
Rob Wilson <rob.wilson195@gmail.com>
2018-10-23 16:32:27 +0100

    Initialize and set up test files.

---
+++ specs/bear_spec.rb
@@ -0,0 +1,9 @@
+require( 'minitest/autorun' )
+require( 'minitest/rg' )
+require_relative( '../bear' )
+require_relative( '../fish' )
+require_relative( '../river' )
+
+class TestBear < MiniTest::Test
+
+end
---
+++ specs/fish_spec.rb
@@ -0,0 +1,7 @@
+require( 'minitest/autorun' )
+require( 'minitest/rg' )
+require_relative( '../fish' )
+
+class TestFish < MiniTest::Test
+
+end
